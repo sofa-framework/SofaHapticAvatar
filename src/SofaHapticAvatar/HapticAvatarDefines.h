f47d9fc1e008673fbd64b2443141b682906c58f6
epernod <erik.pernod@gmail.com>
2019-11-01 20:20:16 +0100

    [src] Add new empty classes for HA defines and HA communication API

---
+++ src/SofaHapticAvatar/HapticAvatarDefines.h
@@ -0,0 +1,19 @@
+/******************************************************************************
+* License version                                                             *
+*                                                                             *
+* Authors:                                                                    *
+* Contact information:                                                        *
+******************************************************************************/
+#ifndef SOFA_HAPTICAVATAR_HAPTICAVATARDEFINES_H
+#define SOFA_HAPTICAVATAR_HAPTICAVATARDEFINES_H
+
+
+namespace HapticAvatar
+{
+
+#define NBJOINT 6
+
+
+} // namespace HapticAvatar
+
+#endif // SOFA_HAPTICAVATAR_HAPTICAVATARDEFINES_H
